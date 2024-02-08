005b66a68d64cd13d9e9da5e976f601a38c02f8f
Dmitriy <morqqulis@gmail.com>
2024-02-08 09:45:26 +0400

    Starting new Project -  Front-end Portfolio

---
+++ src/components/global/Header/Burger.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const Burger = () => {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const toggleMenu = () => {
+		setIsMenuOpen(!isMenuOpen);
+	};
+
+	useEffect(() => {
+		const htmlElement: HTMLElement | null = document.querySelector("html");
+		if (htmlElement) {
+			if (isMenuOpen) {
+				htmlElement.classList.add("menu-opened");
+			} else {
+				htmlElement.classList.remove("menu-opened");
+			}
+			return () => {
+				htmlElement.classList.remove("menu-opened");
+			};
+		}
+	}, [isMenuOpen]);
+
+	return (
+		<button className='burger relative w-[18px] h-4 flex sm:hidden' onClick={toggleMenu} type="button" aria-label="Toggle menu">
+			<span className='h-0.5 w-full absolute block transition-transform duration-300 bg-[#607B96] group-[.menu-opened]:bg-[#FEA55F] top-0 group-[.menu-opened]:-rotate-45 group-[.menu-opened]:top-[50%]'></span>
+			<span className='h-0.5 w-full absolute block transition-transform duration-300 bg-[#607B96] group-[.menu-opened]:bg-[#FEA55F] top-[45%] group-[.menu-opened]:scale-0'></span>
+			<span className='h-0.5 w-full absolute block transition-transform duration-300 bg-[#607B96] group-[.menu-opened]:bg-[#FEA55F] bottom-0 group-[.menu-opened]:rotate-45 group-[.menu-opened]:top-[50%]'></span>
+		</button>
+	);
+};
+export default Burger;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		screens: {
+			ssm: { max: "767.99px" },
+			mmd: { max: "991.99px" },
+			llg: { max: "1199.99px" },
+			xxl: { max: "1399.99px" },
+
+			xs: "489.99px",
+			mid: "575.99px",
+			sm: "767.99px",
+			md: "991.99px",
+			lg: "1199.99px",
+			xl: "1399.99px",
+		},
+		extend: {
+			colors: {
+				"border-color": "#1E2D3D",
+			},
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
